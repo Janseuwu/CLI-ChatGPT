6bad12d2b3b68b6e7604188e5d3711640f651139
Wraaath <mads@wraaath.com>
2023-05-23 11:21:41 +0200

    echo steps

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Setup running"
 
 KEYFILE="${XDG_STATE_HOME:-$HOME/.local/state}/ChatGPT-CLI/openai_api_key"
 
@@ -6,7 +7,11 @@ KEYFILE="${XDG_STATE_HOME:-$HOME/.local/state}/ChatGPT-CLI/openai_api_key"
 read -p 'Enter your OpenAI API key > ' -r API_KEY 
 
 # Store API key
+echo "Storing API-key"
 mkdir -p "$(dirname "$KEYFILE")"
 echo "export OPENAI_API_KEY='$API_KEY'" > "$KEYFILE" # add the API key to the keyfile
 
+echo "Creating symlink from git-repo to ~/.local/bin/gpt"
 ln -s $(realpath gpt) $HOME/.local/bin/gpt # create symlink to ~/.local/bin
+
+echo "Setup done"
